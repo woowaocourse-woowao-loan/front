977862dd737ddf5576c35aa3fbc175884421b8f4
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-15 10:39:46 +0900

    refactor: 로고 확장자 수정

--- src/screens/BookListPage.tsx
+++ src/screens/BookListPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../App.css';
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.svg";
 
 interface Book {
     id: number;
